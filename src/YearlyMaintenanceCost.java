b29669c0c97cef422482e19168b8f5651f3e153e
Steward-Gaven <Stewarwn@mail.uc.edu>
2022-04-27 10:32:45 -0400

    Initial commit

---
+++ src/YearlyMaintenanceCost.java
@@ -0,0 +1,18 @@
+class Main {
+
+    public static void main(String[] args) {
+
+    int Winter = 23000;
+    int Spring = 17000;
+    int Summer = 13000;
+    int Fall = 19000;
+    int total = Winter + Spring + Fall + Summer;
+
+    System.out.println("The maintenance cost during Winter is: " + Winter);
+    System.out.println("The maintenance cost during Spring is: " + Spring);
+    System.out.println("The maintenance cost during Fall is: " + Fall);
+    System.out.println("The maintenance cost during Summer is: " + Summer);
+    System.out.println("The maintenance cost during the entire year is: " + total);
+
+    }
+}
